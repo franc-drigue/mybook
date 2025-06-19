5d2b74c38919cdc87920947fe5ea2621cac6e153
Francisco <francdrigue@gmail.com>
2025-06-18 21:03:55 -0300

    create Adapter and ViewHolder

--- app/src/main/java/com/example/mybooks/ui/HomeFragment.kt
+++ app/src/main/java/com/example/mybooks/ui/HomeFragment.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mybooks.databinding.FragmentHomeBinding
+import com.example.mybooks.ui.adapter.BookAdapter
 import com.example.mybooks.viewmodels.HomeViewModel
 
 class HomeFragment : Fragment() {
@@ -19,6 +21,9 @@ class HomeFragment : Fragment() {
 
     private val homeViewModel: HomeViewModel by viewModels()
 
+    //Forma de instanciar o adapter
+    private val adapter: BookAdapter = BookAdapter()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -26,6 +31,11 @@ class HomeFragment : Fragment() {
     ): View {
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        binding.recyclerviewBooks.layoutManager = LinearLayoutManager(context)
+
+        //adapter
+        binding.recyclerviewBooks.adapter = adapter
 
         return binding.root
     }
---
+++ app/src/main/java/com/example/mybooks/ui/adapter/BookAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.mybooks.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mybooks.databinding.ItemBookBinding
+import com.example.mybooks.entity.BookEntity
+import com.example.mybooks.ui.viewholder.BookViewHolder
+
+class BookAdapter: RecyclerView.Adapter<BookViewHolder>() {
+
+    private val bookList = mutableListOf<BookEntity>()
+
+    // cria o elemento de layout
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): BookViewHolder {
+        val view = ItemBookBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return BookViewHolder(view)
+    }
+
+    // Responsável em vincular os dados com o layout
+    override fun onBindViewHolder(
+        holder: BookViewHolder,
+        position: Int
+    ) {
+        holder.bind(bookList[position])
+    }
+
+    // Retorna quantos elemetos existem na RecyclerView
+    override fun getItemCount(): Int {
+        //poderia usar o bookList.count( )
+        return  bookList.size
+    }
+}
